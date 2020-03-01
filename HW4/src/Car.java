5fc3f6d15371aaf2ac6d71bed3850e917b2be466
SerhiiSokol <sokolovsv91@gmail.com>
2020-03-01 21:24:44 +0200

    Домашнее задание №4, набросок

--- HW4/src/Car.java
+++ HW4/src/Car.java
@@ -1,25 +1,26 @@
 public class Car {
-    double tankVolume,tankBalance,fuelConsumption,fuelCost,refuelingCost;
+    double tankVolume,tankBalance,fuelConsumption,fuelCost,refuelingCost,refueling;
 
     Car (double tankVolume, double tankBalance,double fuelConsumption, double fuelCost){
         this.tankVolume = tankVolume;//объем бака
-        this.tankBalance = tankVolume;//остаток в баке
+        this.tankBalance = tankBalance;//остаток в баке
         this.fuelConsumption = fuelConsumption;//расход топлива на 100 км
         this.fuelCost = fuelCost;// стоимость топлива
+        this.refueling = refueling; // объем дозаправки
     }
 
-    public double fillAFullTank() {                                       //залить полный бак с учетом объема остатка
-        tankBalance = tankVolume;
-        return tankBalance;
+    void fillAFullTank() {                                       //залить полный бак с учетом объема остатка
+        this.tankBalance = this.tankVolume;
+        System.out.println("Заливаем полный бак");
     }
 
     public double determineTheRemaining(double distance) {                                       //определить остаток топлива по преодолении N км
-        this.tankBalance = tankVolume - ((fuelConsumption / 100) * distance);
+        this.tankBalance -= (this.fuelConsumption / 100) * distance;
         System.out.println("Остаток в баке:" +this.tankBalance);
         return this.tankBalance;
     }
 
-    public double determineRefueled() {                                           //определить сколько надо дозаправить топлива при преодолении N км
-      return tankVolume-tankBalance;
+    void determineRefueled() {                                           //определить сколько надо дозаправить топлива при преодолении N км
+      refueling = this.tankBalance-(this.fuelConsumption/100)*this.fuelCost;
     }
 }
