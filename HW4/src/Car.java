a5ece4d6ecbd05e01bbf2d0311f78b886504c9f0
SerhiiSokol <sokolovsv91@gmail.com>
2020-03-08 18:35:56 +0200

    Домашнее задание №4

--- HW4/src/Car.java
+++ HW4/src/Car.java
@@ -1,27 +1,31 @@
-public class Car {
-    double tankVolume,tankBalance,fuelConsumption,fuelCost,refuelingCost,refueling;
+import jdk.swing.interop.SwingInterOpUtils;
 
-    Car (double tankVolume, double tankBalance,double fuelConsumption, double fuelCost){
+public class Car {
+    double tankVolume, tankBalance, fuelConsumption, fuelCost;
+    double costOfTravel = 0;
+
+    Car(double tankVolume, double tankBalance, double fuelConsumption, double fuelCost) {
         this.tankVolume = tankVolume;//объем бака
         this.tankBalance = tankBalance;//остаток в баке
         this.fuelConsumption = fuelConsumption;//расход топлива на 100 км
-        this.fuelCost = fuelCost;// стоимость топлива
-        this.refueling = refueling; // объем дозаправки
+        this.fuelCost = fuelCost;               // стоимость топлива
     }
 
     void fillAFullTank() {                                       //залить полный бак с учетом объема остатка
-        this.tankBalance = this.tankVolume;
-        System.out.println("Заливаем полный бак");
+        tankBalance += tankVolume - tankBalance;
+        System.out.println("Заправляем полный бак");
     }
 
-    public double determineTheRemaining(double distance) {                                       //определить остаток топлива по преодолении N км
-        this.tankBalance -= (this.fuelConsumption / 100) * distance;
-        System.out.println("Остаток в баке:" +this.tankBalance);
-        return this.tankBalance;
+    void determineTheRemaining(double distance) {                                       //определить остаток топлива по преодолении N км
+        tankBalance = tankVolume - (fuelConsumption / 100) * distance;
+        System.out.println("В баке осталось " + Math.round(tankBalance) + " литров топлива");
     }
 
-    void determineRefueled(double distance) {                                           //определить сколько надо дозаправить топлива при преодолении N км
-        refueling = (this.fuelConsumption/100)*distance;
-        System.out.println();
+    void determineRefueled(double distance) {
+        double refueled = (fuelConsumption / 100) * distance;
+        double refuledCost = refueled * fuelCost;                                                                                   //определить сколько надо дозаправить топлива при преодолении N км
+        System.out.println("Необходимо дозаправить " + Math.round(refueled) + " литров топлива, это обойдется в " + Math.round(refuledCost) + " грн.");
+        costOfTravel += refuledCost;
     }
+
 }
--- HW4/src/Main.java
+++ HW4/src/Main.java
@@ -1,10 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        Car vaz = new Car(45,20,8.5,26);
+
+        Car VAZ = new Car(45,20,9,26);
 
         double OdessaKrivoeOzero = 178.8;
         double KrivoeOzeroZhashkov = 152.4;
         double ZhashkovKiev = 151.6;
 
+        System.out.println("Старт поездки, выезжаем с полным баком с Одессы. ");
+        System.out.println("Первая остановка Кривое Озеро.");
+        VAZ.determineTheRemaining(OdessaKrivoeOzero);
+        VAZ.determineRefueled(OdessaKrivoeOzero);
+        VAZ.fillAFullTank();
+        System.out.println("Вторая остановка Жашков.");
+        VAZ.determineTheRemaining(KrivoeOzeroZhashkov);
+        VAZ.determineRefueled(KrivoeOzeroZhashkov);
+        VAZ.fillAFullTank();
+        System.out.println("Завершаем поездку. Киев.");
+        VAZ.determineTheRemaining(ZhashkovKiev);
+        VAZ.determineRefueled(ZhashkovKiev);
+        VAZ.fillAFullTank();
+        System.out.println();
+        System.out.println("Полная стоимость поездки составила " + Math.round(VAZ.costOfTravel) +" грн.");
     }
 }
