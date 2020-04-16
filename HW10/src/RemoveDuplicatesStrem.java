e656db528a6b8e915ffd8ac15d6e39914d21274c
SerhiiSokol <sokolovsv91@gmail.com>
2020-04-16 04:12:05 +0300

    Домашнее задание №7

---
+++ HW10/src/RemoveDuplicatesStrem.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class RemoveDuplicatesStrem {
+    public static void main(String[] args) {
+        var list = new ArrayList<>(Arrays.asList(2, 14, 55, 55, 2, 2, 10, 4, 14, 56, 66, 77));
+        System.out.println("Лист с дублями: " + list);
+        list = (ArrayList<Integer>) list.stream().distinct().collect(Collectors.toList());
+        System.out.println("Без дублей: " + list);
+    }
+}
