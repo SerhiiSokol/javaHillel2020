16b7942b41c4a8036cb01d384976e84c9e77f189
SerhiiSokol <sokolovsv91@gmail.com>
2020-04-16 04:48:05 +0300

    Домашнее задание №15

--- HW15/src/test/java/NumbToGrafTest.java
+++ HW15/src/test/java/NumbToGrafTest.java
@@ -1,15 +1,10 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 class NumbToGrafTest {
-
     @Test
     void numToGraf() {
         char[] ch = {6};
         for (Character i : ch)
             assertEquals(true, true);
-
-
     }
 }
