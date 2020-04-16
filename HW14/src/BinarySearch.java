0ccd794f2ee66e8bf1438e89c16de98bd2bbb15d
SerhiiSokol <sokolovsv91@gmail.com>
2020-04-16 04:46:53 +0300

    Домашнее задание №14

--- HW14/src/BinarySearch.java
+++ HW14/src/BinarySearch.java
@@ -1,18 +1,16 @@
 class BinarySearch {
     // Driver method to test above
     public static void main(String args[]) {
-        BinarySearch ob = new BinarySearch ();
+        BinarySearch ob = new BinarySearch();
         int arr[] = {2, 3, 4, 10, 40};
         int n = arr.length;
         int x = 10;
-        int result = ob.binarySearch (arr, 0, n - 1, x);
+        int result = ob.binarySearch(arr, 0, n - 1, x);
         if (result == -1)
-            System.out.println ("Element not present");
+            System.out.println("Element not present");
         else
-            System.out.println ("Element found at index " + result);
+            System.out.println("Element found at index " + result);
     }
-
-
 
     int binarySearch(int arr[], int l, int r, int x) {
         if (r >= l) {
@@ -22,9 +20,9 @@ class BinarySearch {
                 return mid;
 
             if (arr[mid] > x)
-                return binarySearch (arr, l, mid - 1, x);
+                return binarySearch(arr, l, mid - 1, x);
 
-            return binarySearch (arr, mid + 1, r, x);
+            return binarySearch(arr, mid + 1, r, x);
         }
 
         return -1;
