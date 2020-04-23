f0ec58c2c9432d3a7cbfc61c4380b8ab8f1ab246
SerhiiSokol <sokolovsv91@gmail.com>
2020-04-23 03:41:17 +0300

    HW

--- JavaHillel2020/HW10/src/RemoveDuplicates.java
+++ JavaHillel2020/HW10/src/RemoveDuplicates.java
@@ -5,23 +5,19 @@ import java.util.Set;
 
 public class RemoveDuplicates {
     public static void main(String[] args) {
-        var list = new ArrayList<> (Arrays.asList (2, 14, 55, 55, 2, 2, 10, 4, 14, 56, 66, 77));
-        System.out.println ("Лист с дублями: " + list);
-        System.out.println ("Значение под индексом 5: "+list.get (5));
-
-        removeDuplicates (list);
-
-        System.out.println ("Без дублей: " + list);
-        System.out.println ("Новое значение под индексом 5: "+list.get (5));
+        var list = new ArrayList<>(Arrays.asList("Василий", 14, 55, "Василий", 2, 2, 10, 4, 14, 56, 66, 77));
+        System.out.println("Лист с дублями: " + list);
+        removeDuplicates(list);
+        System.out.println("Без дублей: " + list);
     }
 
     public static <T> void removeDuplicates(ArrayList<T> list) {
 
         Set<T> set = new LinkedHashSet<>(list);
 
-        list.clear ();
+        list.clear();
 
-        list.addAll (set);
+        list.addAll(set);
 
     }
 }
--- JavaHillel2020/HW10/src/RemoveDuplicatesStrem.java
+++ JavaHillel2020/HW10/src/RemoveDuplicatesStrem.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 public class RemoveDuplicatesStrem {
     public static void main(String[] args) {
-        var list = new ArrayList<>(Arrays.asList(2, 14, 55, 55, 2, 2, 10, 4, 14, 56, 66, 77));
+        var list = new ArrayList<>(Arrays.asList(3, 14, 55, 55, 2, 2, 10, 4, 14, 56, 66, 77));
         System.out.println("Лист с дублями: " + list);
         list = (ArrayList<Integer>) list.stream().distinct().collect(Collectors.toList());
         System.out.println("Без дублей: " + list);
