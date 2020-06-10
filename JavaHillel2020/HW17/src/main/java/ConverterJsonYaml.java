3f71c094c88b4c8cbd3a49c08a0a00ee1f8c7a66
SerhiiSokol <sokolovsv91@gmail.com>
2020-06-10 16:54:57 +0300

    HW19

--- JavaHillel2020/HW17/src/main/java/ConverterJsonYaml.java
+++ JavaHillel2020/HW17/src/main/java/ConverterJsonYaml.java
@@ -7,19 +7,19 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
+
 
 
 public class ConverterJsonYaml {
     public static void main(String[] args) throws IOException {
-        toJson(ConverterJsonYaml.startTest());
+        toJson();
         toYaml();
     }
 
-    public static void toJson(String patch) throws IOException {
+    public static void toJson() throws IOException {
         //создаем обьект и присваиваем ему значение мапы файла yaml
         System.out.println("Читаю YAML");
-        Object json = new YAMLMapper().readValue(new FileReader(patch), Object.class);
+        Object json = new YAMLMapper().readValue(new FileReader("C:\\Users\\sokol\\IdeaProjects\\JavaHillel2020\\JavaHillel2020\\HW17\\src\\main\\resources\\myPerson.yaml"), Object.class);
         //создаем файл и записываем в него значение мапы мапу json
         System.out.println("Создаю и записываю JSON");
         new JsonMapper().writerWithDefaultPrettyPrinter().writeValue(new File(
@@ -39,18 +39,6 @@ public class ConverterJsonYaml {
                 "C:\\Users\\sokol\\IdeaProjects\\JavaHillel2020\\JavaHillel2020\\HW17\\src\\main\\resources\\myPerson2.yaml");
         writer.write((yaml).replaceAll("\"", ""));
         writer.flush();
-    }
-
-    static String startTest() {
-        System.out.println("Укажите полный путь вместе именем конвертируемого файла пример:(C:\\\\myPerson.yaml)");
-        Scanner sc = new Scanner(System.in);
-        String patch = sc.nextLine();
-        try {
-            new YAMLMapper().readValue(new FileReader(patch), Object.class);
-        } catch (Exception e) {
-            System.out.println("Неверный путь или имя файла");
-            startTest();
-        }return patch;
     }
 }
 
