2d792a67057868ed00417ef45197e63d2c6f45e5
SerhiiSokol <sokolovsv91@gmail.com>
2020-06-10 18:55:36 +0300

    HW20

--- JavaHillel2020/HW20/src/Fabric.java
+++ JavaHillel2020/HW20/src/Fabric.java
@@ -1,13 +1,14 @@
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class Fabric implements Runnable{
-    int size = 10;
-    final ArrayList<Integer> ar = new ArrayList<>(size);
+    int size = 5;
+    final Stack<Integer> ar = new Stack<>();
 
     public int getSize() {
         return size;
     }
-    public ArrayList<Integer> getAr() {
+    public Stack<Integer> getAr() {
         return ar;
     }
 
@@ -29,6 +30,9 @@ public class Fabric implements Runnable{
             if (getAr().size() == getSize()) {
 
                 try {
+                    if (getAr().size()==getSize()){
+                        System.out.println("Буфер заполнен "+ getAr());
+                    }
                     getAr().wait();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -37,6 +41,7 @@ public class Fabric implements Runnable{
             int newValue = (int) (Math.random()*10);
             getAr().add(newValue);
             getAr().notifyAll();
+
             return newValue;
         }
     }
@@ -46,24 +51,24 @@ public class Fabric implements Runnable{
         if (Thread.currentThread().getName().equals("consumer")) {
             while (true){
                 try {
-                    Thread.sleep(2000);
-                    if (getAr().size()==getSize()){
-                        System.out.println("Буфер заполнен"+ getAr());
-                    }
-                    System.out.println("Удаляю первый элемент " +consumer());
+                    Thread.sleep(1500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                System.out.println("Удаляю первый элемент " +consumer());
+
 
             }
         } else if (Thread.currentThread().getName().equals("producer")) {
+
             while (true){
                 try {
-                    Thread.sleep(1000);
-                    System.out.println("Создаю элемент " +producer());
+                    Thread.sleep(500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+                    System.out.println("Создаю элемент " +producer());
+
             }
         } else System.out.println("нет такого пользователя");
     }
