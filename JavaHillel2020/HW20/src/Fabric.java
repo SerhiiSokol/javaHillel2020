fefd42e94b3ade08eacbb640b7985f0124f7e136
SerhiiSokol <sokolovsv91@gmail.com>
2020-06-10 19:03:40 +0300

    HW20

--- JavaHillel2020/HW20/src/Fabric.java
+++ JavaHillel2020/HW20/src/Fabric.java
@@ -1,75 +1,77 @@
-import java.util.ArrayList;
 import java.util.Stack;
 
-public class Fabric implements Runnable{
+public class Fabric implements Runnable {
     int size = 5;
     final Stack<Integer> ar = new Stack<>();
 
     public int getSize() {
         return size;
     }
+
     public Stack<Integer> getAr() {
         return ar;
     }
 
     private int consumer() {
-    synchronized (getAr()) {
-        if (getAr().isEmpty()) {
-            try {
-                getAr().wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        synchronized (getAr()) {
+            if (getAr().isEmpty()) {
+                try {
+                    getAr().wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
+            getAr().notifyAll();
+            return getAr().remove(0);
         }
-        getAr().notifyAll();
-        return getAr().remove(0);
     }
-}
-    private double producer() {
+
+    private int producer() {
         synchronized (getAr()) {
             if (getAr().size() == getSize()) {
 
                 try {
-                    if (getAr().size()==getSize()){
-                        System.out.println("Буфер заполнен "+ getAr());
+                    if (getAr().size() == getSize()) {
+                        System.out.println("The list is full " + getAr());
                     }
                     getAr().wait();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-            int newValue = (int) (Math.random()*10);
+            int newValue = (int) (Math.random() * 10);
             getAr().add(newValue);
             getAr().notifyAll();
 
             return newValue;
         }
     }
+
     @Override
     public void run() {
 
         if (Thread.currentThread().getName().equals("consumer")) {
-            while (true){
+            while (true) {
                 try {
                     Thread.sleep(1500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                System.out.println("Удаляю первый элемент " +consumer());
+                System.out.println("Delete the first list item " + consumer());
 
 
             }
         } else if (Thread.currentThread().getName().equals("producer")) {
 
-            while (true){
+            while (true) {
                 try {
                     Thread.sleep(500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                    System.out.println("Создаю элемент " +producer());
+                System.out.println("Add items to the end of the list. " + producer());
 
             }
-        } else System.out.println("нет такого пользователя");
+        }
     }
 }
