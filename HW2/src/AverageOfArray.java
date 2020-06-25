8da741e0b90eb373304c9ae48baa1964bd16c63e
SerhiiSokol <sokolovsv91@gmail.com>
2020-06-25 08:58:12 +0300

    HW 2

--- HW2/src/AverageOfArray.java
+++ HW2/src/AverageOfArray.java
@@ -1,24 +1,22 @@
 import java.util.Scanner; // импортируем библиотеку util в которой содержиться класс scanner
 
-public class Average {
+public class AverageOfArray {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);  // обьявляем о создании нового сканера
         System.out.println("Введите количество элементов для расчета среднего значения");
-        int n = sc.nextInt();
-        double[] A = new double[n]; // Создаем массив
-
-        System.out.println("Введите " + n + " значений(я)");
-
-        for (int i = 0; i < n; i++) {
-            A[i] = sc.nextDouble();
+        int length = sc.nextInt();
+        double[] array = new double[length]; // Создаем массив
+        System.out.println("Введите " + length + " значений(я)");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = sc.nextDouble();
         } // Заполняем массив
 
-        double sum = 0;
-        for (double v : A) {
-            sum += v;
+        double averageSum = 0;
+        for (double elementOfArray : array) {
+            averageSum += elementOfArray;
         }
-        double avg = sum / n;  // Проводим расчет
+        double avg = averageSum / array.length;  // Проводим расчет
         System.out.print("Среднее арифметическое введеных значений: " + Math.rint(avg * 100) / 100); //Выводим результат в консоль
     }
 }
